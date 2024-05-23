63b8e80f241670a673ad10f0fbe9c8acad01610c
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-05-23 16:14:51 +0330

    init project

---
+++ server.js
@@ -0,0 +1,46 @@
+const Hapi = require('@hapi/hapi');
+const Path = require('path');
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 3000,
+        host: '0.0.0.0',
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'public')
+            }
+        }
+    });
+
+    await server.register(require('@hapi/inert'));
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return h.file('index.html');
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+            directory: {
+                path: '.',
+                redirectToSlash: true,
+                index: true
+            }
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
